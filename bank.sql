0ea446d4a028e3b7a329fd25d85b9ba4f1ea5f1c
MH15O <76598767+MH15O@users.noreply.github.com>
2021-06-12 23:08:30 +0200

    Add files via upload

---
+++ bank.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 12 juin 2021 à 23:08
+-- Version du serveur :  10.4.18-MariaDB
+-- Version de PHP : 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `connections`
+--
+
+CREATE TABLE `connections` (
+  `id` int(11) NOT NULL,
+  `username` varchar(52) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL,
+  `adresse1` varchar(52) NOT NULL,
+  `adress2` varchar(52) NOT NULL,
+  `date` date NOT NULL,
+  `amount` int(52) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(52) NOT NULL,
+  `password` varchar(52) NOT NULL,
+  `email` varchar(52) NOT NULL,
+  `balance` int(20) NOT NULL,
+  `adresse` varchar(52) NOT NULL,
+  `admin` varchar(52) NOT NULL,
+  `statue` varchar(52) NOT NULL,
+  `fname` varchar(52) NOT NULL,
+  `lname` varchar(52) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `balance`, `adresse`, `admin`, `statue`, `fname`, `lname`) VALUES
+(1, 'root', 'rootP', 'root@root.com', 156454554, '154edf465edb', 'neon', 'admin', 'root', 'root');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `connections`
+--
+ALTER TABLE `connections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `connections`
+--
+ALTER TABLE `connections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
